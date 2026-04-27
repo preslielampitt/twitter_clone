3edd59f95fbcaec21f2b89ee00e9c6a3a8a83396
Preslie Lampitt <preslie.lampitt@gmail.com>
2026-04-27 11:57:41 -0700

    make templating generic

--- main.py
+++ main.py
@@ -20,5 +20,12 @@ async def index(request: Request):
         name='index.html',
     )
 
+@app.get('/login', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name='login.html',
+    )
+
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8080, reload=True)
