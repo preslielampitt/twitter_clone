bc1262bdef8d529b863499ea04760ef4dac489d9
Preslie Lampitt <preslie.lampitt@gmail.com>
2026-04-27 11:40:31 -0700

    add templates

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ Starts a hello world webserver.
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
+# Internal Server Error:
+# always means a python error inside of the function that corresponds to the route
+# or "page" that you were connecting to in firefox
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
-    return 'hello <b>world</b>'
+    return templates.TemplateResponse(
+        request=request,
+        name='index.html',
+    )
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8080, reload=True)
