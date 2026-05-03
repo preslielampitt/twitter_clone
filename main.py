5b6c62d2518d5708ba8a4a4293ade8d8cbff5a09
Preslie Lampitt <preslie.lampitt@gmail.com>
2026-05-03 15:32:06 -0700

    create templates for all pages and add css styling

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ Starts a hello world webserver.
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 import sqlite3
 
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
 # Internal Server Error:
@@ -16,17 +18,28 @@ templates = Jinja2Templates(directory='templates')
 # or "page" that you were connecting to in firefox
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
-    is_logged_in='True'
+    is_logged_in = True
 
-    # exact username from database
+    messages = []
+
     con = sqlite3.connect('twitter_clone.db')
     cur = con.cursor()
     sql = """
-    SELECT username FROM users WHERE id=1;
+    SELECT messages.message, messages.created_at, users.username, users.age
+    FROM messages
+    JOIN users ON messages.sender_id = users.id
+    ORDER BY messages.created_at DESC;
     """
     cur.execute(sql)
     for row in cur.fetchall():
-        username = row[0]
+        message = {
+            'text': row[0],
+            'timestamp': row[1],
+            'username': row[2],
+            'age': row[3],
+        }
+        messages.append(message)
+    con.close()
 
     # create response
     return templates.TemplateResponse(
@@ -34,16 +47,49 @@ async def index(request: Request):
         name='index.html',
         context={
             'is_logged_in': is_logged_in,
-            'username': username,
+            'messages': messages,
         },
     )
 
 @app.get('/login', response_class=HTMLResponse)
-async def index(request: Request):
+async def login(request: Request):
     is_logged_in='True'
     return templates.TemplateResponse(
         request=request,
         name='login.html',
+        context={
+            'is_logged_in': is_logged_in
+        },
+    )
+
+@app.get('/logout', response_class=HTMLResponse)
+async def logout(request: Request):
+    is_logged_in='False'
+    return templates.TemplateResponse(
+        request=request,
+        name='logout.html',
+        context={
+            'is_logged_in': is_logged_in
+        },
+    )
+
+@app.get('/create_message', response_class=HTMLResponse)
+async def create_message(request: Request):
+    is_logged_in='True'
+    return templates.TemplateResponse(
+        request=request,
+        name='create_message.html',
+        context={
+            'is_logged_in': is_logged_in
+        },
+    )
+
+@app.get('/create_user', response_class=HTMLResponse)
+async def create_user(request: Request):
+    is_logged_in='True'
+    return templates.TemplateResponse(
+        request=request,
+        name='create_user.html',
         context={
             'is_logged_in': is_logged_in
         },
